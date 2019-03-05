1369e3c8a075bc43784bfe476abec90fe071e684
Natasha Jaques <jaquesn@mit.edu>
2019-03-05 17:56:03 -0500

    trying to fix int / float error in ray

--- python/ray/rllib/evaluation/metrics.py
+++ python/ray/rllib/evaluation/metrics.py
@@ -35,7 +35,7 @@ def collect_episodes(local_evaluator,
         for a in remote_evaluators
     ]
     collected, _ = ray.wait(
-        pending, num_returns=len(pending), timeout=timeout_seconds * 1.0)
+        pending, num_returns=len(pending), timeout=timeout_seconds)
     num_metric_batches_dropped = len(pending) - len(collected)
 
     metric_lists = ray.get(collected)
